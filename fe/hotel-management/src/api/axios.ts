2d80aaa868be307c58b163aebf376169cacf33f0
NguyenViet25 <vietnqhe172363@fpt.edu.vn>
2025-11-02 18:50:03 +0700

    fix vite buld

--- fe/hotel-management/src/api/axios.ts
+++ fe/hotel-management/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5283/api";
+const API_URL = import.meta.env.VITE_API_URL;
 
 const axiosInstance = axios.create({
   baseURL: API_URL,
