9a7438bd5a88057bd86fe7c77472b5cf02d3be28
NguyenViet25 <vietnqhe172363@fpt.edu.vn>
2025-12-11 02:30:04 +0700

    config global date format

--- fe/hotel-management/src/main.tsx
+++ fe/hotel-management/src/main.tsx
@@ -8,11 +8,23 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "./index.css";
-
 import { AuthProvider } from "./context/AuthContext";
+import "./index.css";
 import router from "./routes";
 import theme from "./theme";
+
+import dayjs from "dayjs";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+// 🔥 Dayjs config — put here
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.tz.setDefault("Asia/Ho_Chi_Minh");
+
+Date.prototype.toJSON = function () {
+  return dayjs(this).format("YYYY-MM-DD HH:mm:ss");
+};
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
