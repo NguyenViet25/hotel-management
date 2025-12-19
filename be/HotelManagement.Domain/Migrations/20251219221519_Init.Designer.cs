dc861d2b2c82a93a35e7c0995911ba7e5d4c6bc7
NguyenViet25 <vietnqhe172363@fpt.edu.vn>
2025-12-20 05:15:32 +0700

    commit

--- be/HotelManagement.Domain/Migrations/20251219221519_Init.Designer.cs
+++ be/HotelManagement.Domain/Migrations/20251219221519_Init.Designer.cs
@@ -12,7 +12,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelManagement.Domain.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20251219210615_Init")]
+    [Migration("20251219221519_Init")]
     partial class Init
     {
         /// <inheritdoc />
